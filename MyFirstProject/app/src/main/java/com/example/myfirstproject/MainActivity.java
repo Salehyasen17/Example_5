2ecc750c49f864e725e28ffcd201da04f1d11ed5
Salehyasen17 <salehyasen17@gmail.com>
2024-10-10 15:15:30 +0300

    First Commit

--- MyFirstProject/app/src/main/java/com/example/myfirstproject/MainActivity.java
+++ MyFirstProject/app/src/main/java/com/example/myfirstproject/MainActivity.java
@@ -11,19 +11,17 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-private EditText y;
+    private EditText y;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         y = findViewById(R.id.Text);
         }
     public void changeText(View view){
-        y.setText("Saleh");
-
+        y.setText("Welcome To Earth");
     }
-    public void finish(View view){
+    public void Exit(View view){
         finish();
         System.exit(0);
     }
